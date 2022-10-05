aaa62a76a71a9738e0d19915bdf869b12359ad41
BeshoSmile <beshosmile@gmail.com>
2022-10-05 10:22:43 +0200

    add the reverse proxy endpoint

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://adffe6751777b484fb7688cd6b27168b-1236140183.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
