1ef7f63c3be9e71379d0927ac6ad73faeb2d3b36
BeshoSmile <beshosmile@gmail.com>
2022-10-05 10:40:11 +0200

    change env link for api host in frontend

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://adffe6751777b484fb7688cd6b27168b-1236140183.us-east-1.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://ac9debcdbbf8d4d47bc093557532d7f5-2090879221.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
